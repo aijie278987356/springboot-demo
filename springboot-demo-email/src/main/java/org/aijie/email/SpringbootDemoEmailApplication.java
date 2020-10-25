4e9a69a0c99c0abdca3eb0d39735ff4d56dd477c
dingaijie <13920473106@163.com>
2020-10-25 19:53:15 +0800

    发送右键service

---
+++ springboot-demo-email/src/main/java/org/aijie/email/SpringbootDemoEmailApplication.java
@@ -0,0 +1,13 @@
+package org.aijie.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoEmailApplication.class, args);
+	}
+
+}
