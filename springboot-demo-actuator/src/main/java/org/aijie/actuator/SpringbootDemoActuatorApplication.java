a8a1a9ec3a1aa3e51fd26ddc9a7300ce0f9802a5
dingaijie <13920473106@163.com>
2020-11-21 11:51:16 +0800

    1:日志切面

---
+++ springboot-demo-actuator/src/main/java/org/aijie/actuator/SpringbootDemoActuatorApplication.java
@@ -0,0 +1,13 @@
+package org.aijie.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoActuatorApplication.class, args);
+	}
+
+}
---
+++ springboot-demo-actuator/src/test/java/org/aijie/actuator/SpringbootDemoActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aijie.actuator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
